4752edb06db6dc1dfe85e38a4a956a766fa8ef38
Brittany F <brittfranklin92@gmail.com>
2019-02-25 23:43:16 -0500

    More JS

--- assets/javscript/app.js
+++ assets/javscript/app.js
@@ -14,20 +14,43 @@
   }
   On results page, display total correct and incorrect answers
 */
+var userGuess;
 
-$('button').click(function(){
-  window.location.href = 'start.html';
-})
+var correctAnswer;
+
+var wins;
+
+var losses;
 
 var timeLeft = 45;
+
+var count = setInterval(decreaseTime, 1000);
 
 function decreaseTime(){
   timeLeft--;
 
-  //if (timeLeft === 0) {
-
-  //}
+  if (timeLeft === 0) {
+    window.location.href = 'results.html';
+  }
 }
 
 var $timeDiv = $('<div>').addClass('time-left').html('<h3>Time Left: ' + timeLeft + ' seconds</h3>');
   $('body').prepend($timeDiv);
+
+  /*
+  if(answers === userGuess){
+    increment wins by one
+  } else {
+    increment losses by one
+  }
+
+  var $results = $('<div>');
+
+  var $correct = $('<p>').text('Correct: ' + wins);
+
+  var $incorrect = $('<p>).text('Incorrect: ' + losses);
+
+  $results.append($correct, $incorrect);
+  
+  $('body').append($results);
+  */
